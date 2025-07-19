e19b8c94e1894e3b64f55565ba91fdf4a8a31966
Ahad Chowdhury <ahad@maacpiash.com>
2025-07-19 19:27:54 +1000

    🛂 Add user auth via NextAuth.js
    GitHub has been added as the sole identity provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
 	return (
 		<>
-			<header></header>
+			<header>
+				<form action="/api/auth/signin" method="POST">
+					<input type="hidden" name="provider" value="github" />
+					<button
+						type="submit"
+						className="px-4 py-2 bg-black text-white rounded hover:bg-gray-800 transition"
+					>
+						Sign in with GitHub
+					</button>
+				</form>
+			</header>
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 				<article className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 					<h3>Hello, Employer!</h3>
